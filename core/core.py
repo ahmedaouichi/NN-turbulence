3af884c3403bbf91a2522fcf1028b73a8a9cba5f
HashPac <ahmed_aouichi@live.nl>
2019-11-27 10:25:21 +0100

    main opgezet

--- core/core.py
+++ core/core.py
@@ -2,24 +2,15 @@ import numpy as np
 import csv
 
 class Core:
-    
-    counter = 0
-    
+
+    #counter = 0
+
     def __init__(self):
         self.object_id = Core.counter
-        Core.counter += 1
+        #Core.counter += 1
 
-    def calc_S_R(grad_u, k, eps, n):
-
-        S = np.zeros((n, 3, 3))
-        R = np.zeros((n, 3, 3))
-        for i in range(n):
-            S[i, :, :] = k[i]/eps[i] * 0.5 * (grad_u[i, :, :] + np.transpose(grad_u[i, :, :]))
-            R[i, :, :] = k[i]/eps[i] * 0.5 * (grad_u[i, :, :] - np.transpose(grad_u[i, :, :]))
-            
-    
     #### Input: path to file relative to core.py file
-    #### Output: 
+    #### Output:
     def loadData(self, filepath):
 
         with open(filepath, 'r') as f:
@@ -32,20 +23,30 @@ class Core:
                 if row:
                     data_list[:,i] = np.array([float(ii) for ii in row])
         print(i)
-        data_list = data_list[:,:i+1] 
+        data_list = data_list[:,:i+1]
         data = {}
         for j,var in enumerate(names):
             data[var] = data_list[j,:]
-                    
+
         self.data = data
-        
-    
-    def calculateGradient(self):
+
+    def calc_S_R(grad_u, k, eps, n):
+
+        S = np.zeros((n, 3, 3))
+        R = np.zeros((n, 3, 3))
+        for i in range(n):
+            S[i, :, :] = k[i]/eps[i] * 0.5 * (grad_u[i, :, :] + np.transpose(grad_u[i, :, :]))
+            R[i, :, :] = k[i]/eps[i] * 0.5 * (grad_u[i, :, :] - np.transpose(grad_u[i, :, :]))
+
+    return S,R
+
+
+    def calc_gradient(self):
         data = self.data
         x = data['Y']
         y = data['Z']
-        
-        
+
+
         x_small = np.zeros(len(x))
         y_small = np.zeros(len(y))
         counter=0
@@ -53,23 +54,21 @@ class Core:
             if (x[ii] != x[ii+1]):
                 x_small[counter] = x[ii]
                 counter+=1
-        
+
         counter=0
         for ii in range(len(y)-1):
             if (y[ii] != y[ii+1]):
                 y_small[counter] = y[ii]
                 counter+=1
-            
-            
+
+
         x_small = x_small[0:counter+1]
-        
+
         u = np.zeros([3,len(x_small)])
         u[0,:] = data['um']
         #u[1,:] = data['vm']
         #u[2,:] = data['wm']
         u_prime_x = np.zeros(len(x_small))
         for ii in range(len(x_small-1)):
-            u_prime_x[ii] = (u[0,ii*len(x_small)]-u[0,ii*len(x_small)])/(x_small[2]-x_small[0]) 
+            u_prime_x[ii] = (u[0,ii*len(x_small)]-u[0,ii*len(x_small)])/(x_small[2]-x_small[0])
         print(u_prime)
-        
-        
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-from core import Core
+# from core import Core
 from nn import NN
 
 def main():
-    test3 = Core()
-    test3.loadData('../inversion/DATA/SQUAREDUCT/DATA/03500_full.csv')
-    test3.calculateGradient()
-    
-    data = test3.data
-    
+    # core = Core()
+    # core.loadData('../inversion/DATA/SQUAREDUCT/DATA/03500_full.csv')
+    # tau = core.get_tau()
+    # u = core.get_u()
+    # grad_u = core.calc_gradient()
+    # k = core.calc_k()
+    # eps = core.calc_epsilon()
+    # n = core.get_n()
+    # S,R = core.calc_S_R(grad_u, k, eps, n)
+    # eigen_values = core.calc_eigenvalues()
+
+    nn = NN()
+
+    # nn.set_input(eigen_values)
+    # nn.set_num_hidden_layers(num_layers)
+    # nn.set_num_hidden_nodes(num_nodes)
+    # nn.set_num_in_nodes(num_in_nodes)
+    # nn.set_num_out_nodes(num_out_nodes)
+    # nn.learning_rate(learning_rate)
+    #
+    # output = nn.train()
+    # predict_tau = nn.calc_output(output)
+
 main()
