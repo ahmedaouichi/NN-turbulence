aed23beb69dc0acb31bb032ca214b8dedb5c5ea3
13751 <izak.deheer@gmail.com>
2019-12-19 10:27:16 +0100

    Training model using multiple datasets

--- main.py
+++ main.py
@@ -22,95 +22,97 @@ def main():
     core = Core()
 
     ## Specify usecase: (RA)_(Retau), RA = ratio aspect, Retau is usecase = '3_180'
+    
+    eigenvalues_shape = 5
+    tensorbasis_shape = 10
+    stresstensor_shape = 9
 
     Retau = 180
     RA_list = [1,3,5,7,10,14]
     velocity_comps = ['U', 'V', 'W']
     DIM = len(velocity_comps)
 
-    RA = RA_list[0]
-    usecase =  str(RA)+'_'+str(Retau)
 
-    print('--> Import coordinate system')
-    ycoord, DIM_Y = Core.importCoordinates('y', usecase)
-    zcoord, DIM_Z = Core.importCoordinates('z', usecase)
-
-    print('--> Import mean velocity field')
-    data = np.zeros([DIM_Y, DIM_Z, DIM])
-    for ii in range(DIM):
-        data[:,:,ii] = core.importMeanVelocity(DIM_Y, DIM_Z, usecase, velocity_comps[ii])
-
-
-    print('--> Compute mean velocity gradient')
-    velocity_gradient = core.gradient(data, ycoord, zcoord)
-    print('--> Import Reynolds stress tensor')
-    stresstensor = core.importStressTensor(usecase, DIM_Y, DIM_Z)
+    print('--> Build network')
+    neural_network = NN(8, 15, eigenvalues_shape, tensorbasis_shape, stresstensor_shape)
+    neural_network.build()
+    
+    counter=0
+    use_cases = [0,1]
+    for RA in  RA_list[0:2]:
+        print('Import dataset '+str(counter+1)+'/'+str(len(use_cases)))
+        usecase =  str(RA)+'_'+str(Retau)
+    
+        print('--> Import coordinate system')
+        ycoord, DIM_Y = Core.importCoordinates('y', usecase)
+        zcoord, DIM_Z = Core.importCoordinates('z', usecase)
+    
+        print('--> Import mean velocity field')
+        data = np.zeros([DIM_Y, DIM_Z, DIM])
+        for ii in range(DIM):
+            data[:,:,ii] = core.importMeanVelocity(DIM_Y, DIM_Z, usecase, velocity_comps[ii])
+    
+    
+        print('--> Compute mean velocity gradient')
+        velocity_gradient = core.gradient(data, ycoord, zcoord)
+        print('--> Import Reynolds stress tensor')
+        stresstensor = core.importStressTensor(usecase, DIM_Y, DIM_Z)
+    
+        ############ Preparing network inputs #####################################
+    
+        print('--> Compute omega field')
+        eps = np.ones([DIM_Y, DIM_Z]) ## For now using epsilon=1 everywhere
+        print('--> Compute k field')
+        k = core.calc_k(velocity_gradient)
+        print('--> Compute rotation rate and strain rate tensors')
+        S,R = core.calc_S_R(velocity_gradient, k, eps)
+        print('--> Compute eigenvalues for network input')
+        eigenvalues = core.calc_scalar_basis(S, R)  # Scalar basis lamba's
+        print('--> Compute tensor basis')
+        tensorbasis = core.calc_tensor_basis(S, R)
+        
+        ## Reshape 2D array to 1D arrays. Network only takes 1D arrays
+        stresstensor = np.reshape(stresstensor, (-1, 9))
+        tensorbasis = np.reshape(tensorbasis, (-1, 10, 9))
+        eigenvalues = np.reshape(eigenvalues, (-1, 5))
+        
+        ## Input scaling
+        scalingfactor = np.max(stresstensor)
+        shiftfactor = np.mean(stresstensor)
+        stresstensor = stresstensor/scalingfactor
+        
+        print('--> Train network')
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+        neural_network.model.fit([tensorbasis, eigenvalues], stresstensor, batch_size = 500, epochs = 20, verbose = 1, shuffle=True, validation_split=0.8, callbacks=[early_stopping])
+        
+        counter+=1
 
     ############# Visualization ###############################################
 
     ## 3rd input argument refers to one of velocity components <U, V, W>
-    core.plotMeanVelocityComponent(RA_list, Retau, 'U')
+#    core.plotMeanVelocityComponent(RA_list, Retau, 'U')
 
     # core.plotMeanVelocityField(RA, Retau, data, ycoord, zcoord)
 
-    ############ Preparing network inputs #####################################
-
-    print('--> Compute omega field')
-    eps = np.ones([DIM_Y, DIM_Z]) ## For now using epsilon=1 everywhere
-    print('--> Compute k field')
-    k = core.calc_k(velocity_gradient)
-    print('--> Compute rotation rate and strain rate tensors')
-    S,R = core.calc_S_R(velocity_gradient, k, eps)
-    print('--> Compute eigenvalues for network input')
-    eigenvalues = core.calc_scalar_basis(S, R)  # Scalar basis lamba's
-    print('--> Compute tensor basis')
-    tensorbasis = core.calc_tensor_basis(S, R)
-
-    eigenvalues_shape = 5
-    tensorbasis_shape = 10
-    stresstensor_shape = 9
-    
-    ## Input scaling
-    scalingfactor = np.max(stresstensor)
-    shiftfactor = np.mean(stresstensor)
-    stresstensor = stresstensor/scalingfactor
-    
-    ## Reshape 2D array to 1D arrays. Network only takes 1D arrays
-    stresstensor = np.reshape(stresstensor, (-1, 9))
-    tensorbasis = np.reshape(tensorbasis, (-1, 10, 9))
-    eigenvalues = np.reshape(eigenvalues, (-1, 5))
-    
-
-    ############  Building neural network  ####################################
-    
-    print('--> Build network')
-    neural_network = NN(8, 30, eigenvalues_shape, tensorbasis_shape, stresstensor_shape)
-    neural_network.build(eigenvalues, tensorbasis, stresstensor)
-    
-    print('--> Train network')
-    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
-    neural_network.model.fit([tensorbasis, eigenvalues], stresstensor, batch_size = 128, epochs = 20, verbose = 1, shuffle=True, validation_split=0.8, callbacks=[early_stopping])
-    
     print('--> Evalutate model')
-    results = neural_network.model.evaluate([tensorbasis, eigenvalues], stresstensor, batch_size=128)
-    print('test loss, test acc:', results)
+#    results = neural_network.model.evaluate([tensorbasis, eigenvalues], stresstensor, batch_size=128)
+#    print('test loss, test acc:', results)
+    
+    prediction =  neural_network.model.predict([tensorbasis, eigenvalues])
+    prediction = prediction*scalingfactor
     
     
-#    prediction =  neural_network.model.predict()
-#    prediction = prediction*scalingfactor
-#    
-#    
-#    neural_network.plot_results(prediction, stresstensor)
-#    
-#    
-#    ###############  Visualize prediction  ####################################
-#    core.tensorplot(stresstensor, DIM_Y, DIM_Z)
-#    core.tensorplot(prediction, DIM_Y, DIM_Z)
-#    
-#    from keras.utils.vis_utils import plot_model
-#    
-#    plot_model(neural_network.model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-#    
-#    plt.show()    
+    neural_network.plot_results(prediction, stresstensor)
+    
+    
+    ###############  Visualize prediction  ####################################
+    core.tensorplot(stresstensor, DIM_Y, DIM_Z)
+    core.tensorplot(prediction, DIM_Y, DIM_Z)
+    
+    from keras.utils.vis_utils import plot_model
+    
+    plot_model(neural_network.model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    
+    plt.show()
      
 main()
--- nn/nn.py
+++ nn/nn.py
@@ -13,14 +13,14 @@ class NN:
         self.output_shape = output_shape # Shape of b
 
 
-    def build(self, eigenvalues, tensorbasis, stresstensor):
-        alpha = 0.1
+    def build(self):
+        alpha = 0.4
         
         g_input = keras.layers.Input(shape=(self.num_inputs,))
         hidden = keras.layers.Dense(self.num_nodes)(g_input)
         hidden = keras.layers.LeakyReLU(alpha)(hidden)
         
-        for i in range(4):#self.num_layers-1
+        for i in range(self.num_inputs-1):#self.num_layers-1
             hidden = keras.layers.Dense(self.num_nodes)(hidden)
             hidden = keras.layers.LeakyReLU(alpha)(hidden)
         g_output = keras.layers.Dense(self.num_outputs)(hidden)
@@ -30,7 +30,7 @@ class NN:
          # T.batched_tensordot(inputs[0], inputs[1], axes=[[1], [1]])
         multiply_layer = keras.layers.Dot([1,1],normalize=False)([tensor_in, g_output])
         
-        keras.optimizers.SGD(lr=2.5e-2, momentum=0.0001, decay=0, nesterov=False, clipvalue=0.5)
+        keras.optimizers.SGD(lr=2.5e-2, momentum=0.01, decay=0, nesterov=False, clipvalue=0.5)
         model = keras.models.Model(inputs=[tensor_in, g_input], outputs=multiply_layer)
         model.compile(loss = 'mean_squared_error', optimizer = 'adam', metrics = ['accuracy'])
         
