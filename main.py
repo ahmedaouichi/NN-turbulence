3b3d87356db00673aa997684b2b00105b5e93229
HashPac <ahmed_aouichi@live.nl>
2019-12-24 22:25:24 +0100

    b training ipv stress

--- main.py
+++ main.py
@@ -2,14 +2,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 from nn import NN
 from core import Core
+from keras.models import Sequential
+from keras.layers import Dense, Lambda
+from keras.callbacks import EarlyStopping
+from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+import keras.backend as K
+import math as m
 
-########################## Rectangular Duct data ##############################
+def a(x,y):
+    return x**2*y**2
+
+def b(x,y):
+    return x*y
+
+def c(x,y):
+    return y**3*x**3
+
+#
+def plot_results(predicted_stresses, true_stresses):
+
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
+    on_diag = [0, 4, 8]
+    for i in range(9):
+            plt.subplot(3, 3, i+1)
+            ax = fig.gca()
+            ax.set_aspect('equal')
+            plt.plot([-1., 1.], [-1., 1.], 'r--')
+            plt.scatter(true_stresses[:, i], predicted_stresses[:, i])
+            plt.xlabel('True value')
+            plt.ylabel('Predicted value')
+            if i in on_diag:
+                plt.xlim([-1./3., 2./3.])
+                plt.ylim([-1./3., 2./3.])
+            else:
+                plt.xlim([-0.5, 0.5])
+                plt.ylim([-0.5, 0.5])
+    plt.show()
 
 def main():
-
     core = Core()
 
     ## Specify usecase: (RA)_(Retau), RA = ratio aspect, Retau is usecase = '3_180'
+    eigenvalues_shape = 5
+    tensorbasis_shape = 10
+    stresstensor_shape = 9
 
     Retau = 180
     RA_list = [1,3,5,7,10,14]
@@ -28,25 +66,19 @@ def main():
     for ii in range(DIM):
         data[:,:,ii] = core.importMeanVelocity(DIM_Y, DIM_Z, usecase, velocity_comps[ii])
 
-
     print('--> Compute mean velocity gradient')
     velocity_gradient = core.gradient(data, ycoord, zcoord)
     print('--> Import Reynolds stress tensor')
     stresstensor = core.importStressTensor(usecase, DIM_Y, DIM_Z)
-
-    ############# Visualization ###############################################
-
-    ## 3rd input argument refers to one of velocity components <U, V, W>
-    core.plotMeanVelocityComponent(RA_list, Retau, 'U')
-
-    # core.plotMeanVelocityField(RA, Retau, data, ycoord, zcoord)
+    stresstensor = np.reshape(stresstensor, (-1, 3, 3))
 
     ############ Preparing network inputs #####################################
 
     print('--> Compute omega field')
-    eps = np.ones([DIM_Y, DIM_Z]) ## For now using epsilon=1 everywhere
+    eps = 1*np.ones([DIM_Y, DIM_Z]) ## For now using epsilon=1 everywhere
     print('--> Compute k field')
-    k = core.calc_k(velocity_gradient)
+    k = core.calc_k(stresstensor)
+    k = np.reshape(k, (DIM_Y, DIM_Z))
     print('--> Compute rotation rate and strain rate tensors')
     S,R = core.calc_S_R(velocity_gradient, k, eps)
     print('--> Compute eigenvalues for network input')
@@ -54,23 +86,141 @@ def main():
     print('--> Compute tensor basis')
     tensorbasis = core.calc_tensor_basis(S, R)
 
-    eigenvalues_shape = 5
-    tensorbasis_shape = 10
-    stresstensor_shape = 9
-    
+
     ## Reshape 2D array to 1D arrays. Network only takes 1D arrays
+    k = np.reshape(k, -1)
+    b = core.calc_output(stresstensor, k)
     stresstensor = np.reshape(stresstensor, (-1, 9))
     tensorbasis = np.reshape(tensorbasis, (-1, 10, 9))
     eigenvalues = np.reshape(eigenvalues, (-1, 5))
-    
-    ############  Building neural network  ####################################
-    
+    b = np.reshape(b, (-1, 9))
+
     print('--> Build network')
-    nn_3d = NN(8, 30, eigenvalues_shape, tensorbasis_shape, stresstensor_shape)
+    neural_network = NN(8, 30, eigenvalues.shape[1], tensorbasis.shape[1], b.shape[1])
+    neural_network.build()
+
+
     print('--> Train network')
-    nn_3d.build(eigenvalues, tensorbasis, stresstensor)
-    print('--> Evalutate model')
-    result =  nn_3d.model.predict([tensorbasis, eigenvalues])
-    nn_3d.plot_results(result, stresstensor)
-     
+    prediction = neural_network.train(eigenvalues, tensorbasis, b)
+
+    plot_results(prediction, b)
+
+    core.tensorplot(stresstensor, DIM_Y, DIM_Z)
+    predicted_stress = core.calc_tensor(prediction, k)
+    core.tensorplot(predicted_stress, DIM_Y, DIM_Z)
+
+    ###############  Visualize prediction  ####################################
+
+#    from keras.utils.vis_utils import plot_model
+#
+#    plot_model(neural_network.model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+    plt.show()
+
 main()
+
+
+# def load_test_data():
+#     data = np.loadtxt("nn/test_data.txt", skiprows=4)
+#     k = data[:, 0]
+#     eps = data[:, 1]
+#     grad_u_flat = data[:, 2:11]
+#     stresses_flat = data[:, 11:]
+#
+#     num_points = data.shape[0]
+#     grad_u = np.zeros((num_points, 3, 3))
+#     stresses = np.zeros((num_points, 3, 3))
+#     for i in range(3):
+#         for j in range(3):
+#             grad_u[:, i, j] = grad_u_flat[:, i*3+j]
+#             stresses[:, i, j] = stresses_flat[:, i*3+j]
+#
+#     return k, eps, grad_u, stresses
+
+#
+# def make_realizable(labels):
+#         numPoints = labels.shape[0]
+#         A = np.zeros((3, 3))
+#         for i in range(numPoints):
+#             # Scales all on-diags to retain zero trace
+#             if np.min(labels[i, [0, 4, 8]]) < -1./3.:
+#                 labels[i, [0, 4, 8]] *= -1./(3.*np.min(labels[i, [0, 4, 8]]))
+#             if 2.*np.abs(labels[i, 1]) > labels[i, 0] + labels[i, 4] + 2./3.:
+#                 labels[i, 1] = (labels[i, 0] + labels[i, 4] + 2./3.)*.5*np.sign(labels[i, 1])
+#                 labels[i, 3] = (labels[i, 0] + labels[i, 4] + 2./3.)*.5*np.sign(labels[i, 1])
+#             if 2.*np.abs(labels[i, 5]) > labels[i, 4] + labels[i, 8] + 2./3.:
+#                 labels[i, 5] = (labels[i, 4] + labels[i, 8] + 2./3.)*.5*np.sign(labels[i, 5])
+#                 labels[i, 7] = (labels[i, 4] + labels[i, 8] + 2./3.)*.5*np.sign(labels[i, 5])
+#             if 2.*np.abs(labels[i, 2]) > labels[i, 0] + labels[i, 8] + 2./3.:
+#                 labels[i, 2] = (labels[i, 0] + labels[i, 8] + 2./3.)*.5*np.sign(labels[i, 2])
+#                 labels[i, 6] = (labels[i, 0] + labels[i, 8] + 2./3.)*.5*np.sign(labels[i, 2])
+#
+#             # Enforce positive semidefinite by pushing evalues to non-negative
+#             A[0, 0] = labels[i, 0]
+#             A[1, 1] = labels[i, 4]
+#             A[2, 2] = labels[i, 8]
+#             A[0, 1] = labels[i, 1]
+#             A[1, 0] = labels[i, 1]
+#             A[1, 2] = labels[i, 5]
+#             A[2, 1] = labels[i, 5]
+#             A[0, 2] = labels[i, 2]
+#             A[2, 0] = labels[i, 2]
+#             evalues, evectors = np.linalg.eig(A)
+#             if np.max(evalues) < (3.*np.abs(np.sort(evalues)[1])-np.sort(evalues)[1])/2.:
+#                 evalues = evalues*(3.*np.abs(np.sort(evalues)[1])-np.sort(evalues)[1])/(2.*np.max(evalues))
+#                 A = np.dot(np.dot(evectors, np.diag(evalues)), np.linalg.inv(evectors))
+#                 for j in range(3):
+#                     labels[i, j] = A[j, j]
+#                 labels[i, 1] = A[0, 1]
+#                 labels[i, 5] = A[1, 2]
+#                 labels[i, 2] = A[0, 2]
+#                 labels[i, 3] = A[0, 1]
+#                 labels[i, 7] = A[1, 2]
+#                 labels[i, 6] = A[0, 2]
+#             if np.max(evalues) > 1./3. - np.sort(evalues)[1]:
+#                 evalues = evalues*(1./3. - np.sort(evalues)[1])/np.max(evalues)
+#                 A = np.dot(np.dot(evectors, np.diag(evalues)), np.linalg.inv(evectors))
+#                 for j in range(3):
+#                     labels[i, j] = A[j, j]
+#                 labels[i, 1] = A[0, 1]
+#                 labels[i, 5] = A[1, 2]
+#                 labels[i, 2] = A[0, 2]
+#                 labels[i, 3] = A[0, 1]
+#                 labels[i, 7] = A[1, 2]
+#                 labels[i, 6] = A[0, 2]
+#
+#         return labels
+#
+# def plot_tensors(predicted, true):
+#
+#     predicted.reshape()
+#
+#
+# def main():
+#
+#     core = Core()
+#     k, eps, grad_u, stresses = load_test_data()
+#     S, R = core.calc_S_R_test(grad_u, k, eps)
+#     input = core.calc_scalar_basis_test(S, R)  # Scalar basis lamba's
+#     T = core.calc_T_test(S, R)  # Tensor basis T
+#     b = core.calc_output(stresses, k)  # b vector from tau tensor
+#     nn = NN(2, 20, input.shape[1], T.shape[1], b.shape[1])
+#     nn.build()
+#
+#     print(input.shape)
+#     print(T.shape)
+#     print(b.shape)
+#     print(stresses.shape)
+#
+#     # for i in range(50):
+#     #         b = make_realizable(b)
+#
+#     result = nn.train(input, T, b)
+#
+#     # for i in range(50):
+#     #         result = make_realizable(result)
+#
+#     # plot_tensors(result, b)
+#     plot_results(result, b)
+#
+# main()
--- nn/nn.py
+++ nn/nn.py
@@ -1,10 +1,14 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import plot_model
+from keras.layers.normalization import BatchNormalization
+from keras import optimizers
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+import matplotlib.pyplot as plt
 import numpy as np
 import theano.tensor as T
-import matplotlib.pyplot as plt
-
 
 class NN:
 
@@ -15,44 +19,32 @@ class NN:
         self.num_outputs = num_outputs  # Number of tensors in the tensor basis
         self.output_shape = output_shape # Shape of b
 
-    def build(self, invariants, tb, b):
+    def build(self):
+        #model:
         g_input = keras.layers.Input(shape=(self.num_inputs,))
         hidden = keras.layers.Dense(self.num_nodes, activation='relu')(g_input)
         for i in range(self.num_layers-1):
             hidden = keras.layers.Dense(self.num_nodes, activation='relu')(hidden)
         g_output = keras.layers.Dense(self.num_outputs, activation='relu')(hidden)
-
         tensor_in = keras.layers.Input(shape=(self.num_outputs, 9,))
-         # T.batched_tensordot(inputs[0], inputs[1], axes=[[1], [1]])
-        multiply_layer = keras.layers.Dot([1,1], normalize=False)([tensor_in, g_output])
-        b_output = keras.layers.Dense(9)(multiply_layer)
-
-        model = keras.models.Model(inputs=[tensor_in, g_input], outputs=b_output)
-        model.compile(loss = 'mean_squared_error', optimizer = 'sgd', metrics = ['accuracy'])
-        model.fit([tb, invariants], b, batch_size = 200000, epochs = 100, verbose = 0)
+        merge_layer = keras.layers.Dot([1,1], normalize=True)([tensor_in, g_output])
+        normalize_layer = keras.layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)(merge_layer)
+        model = keras.models.Model(inputs=[tensor_in, g_input], outputs=normalize_layer)
+        # optimizer = keras.optimizers.RMSprop(learning_rate=0.009)
+        optimizer = optimizers.SGD(lr=5, decay=0, momentum=0, nesterov=True)
+        model.compile(loss = 'mean_squared_error', optimizer = optimizer, metrics = ['accuracy'])
         self.model = model
-    
-    def plot_results(self,predicted_stresses, true_stresses):
-    
-        components = ['uu', 'vu', 'wu', 'uv', 'vv', 'wv', 'uw', 'vw', 'ww']
-        
-        fig = plt.figure()
-        fig.patch.set_facecolor('white')
-        on_diag = [0, 4, 8]
-        for i in range(9):
-                plt.subplot(3, 3, i+1)
-                ax = fig.gca()
-                ax.set_aspect('equal')
-                plt.plot([-1., 1.], [-1., 1.], 'r--')
-                plt.scatter(true_stresses[:, i], predicted_stresses[:, i])
-                plt.xlabel('True value')
-                plt.ylabel('Predicted value')
-                plt.title(components[i])
-#                if i in on_diag:
-#                    plt.xlim([-1e-2, 1e-2])
-#                    plt.ylim([-1e-2, 1e-2])
-#                else:
-#                    plt.xlim([-1e-2, 1e-2])
-#                    plt.ylim([-1e-2, 1e-2])
-        plt.tight_layout()
-        plt.show()
+
+    def train(self, invariants, tb, b):
+        history = self.model.fit([tb, invariants], b, batch_size = 200, nb_epoch = 10, verbose = 1)
+        # SVG(model_to_dot(self.model).create(prog='dot', format='svg'))
+        plot_model(self.model,show_shapes=True, expand_nested=True, to_file='model.png')
+        result = self.model.predict([tb, invariants])
+        SVG(model_to_dot(self.model, show_shapes=True, show_layer_names=False, rankdir='TB').create(prog='dot', format='svg'))
+        print(self.model.summary())
+        return result
+
+
+
+    # def loss_function(self, y_true, y_pred):
+    #     return np.sqrt(np.mean(np.square(y_true-y_pred)))
